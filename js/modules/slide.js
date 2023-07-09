bec9c5039f947e8ab0f2ccc857322e12e5ba58e9
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2023-07-09 16:16:43 -0300

    Cálculos no objeto dist corrigido

--- js/modules/slide.js
+++ js/modules/slide.js
@@ -5,19 +5,30 @@ export default class Slide {
     this.dist = { startX: 0, moviment: 0, finalPosition: 0 };
   }
 
+  moveSlide(distX) {
+    this.dist.movePosition = distX;
+    this.slide.style.transform = `translateX(${distX}px)`;
+  }
+
+  updatePosition(pageX) {
+    this.dist.moviment = (pageX - this.dist.startX) * 1.6;
+    return this.dist.finalPosition + this.dist.moviment;
+  }
+
   onMove(event) {
-    this.dist.moviment = event.pageX - this.dist.startX;
-    this.slide.style.transform = `translateX(${this.dist.moviment}px)`;
+    const finalPosition = this.updatePosition(event.pageX);
+    this.moveSlide(finalPosition);
   }
 
   onEnd() {
     this.wrapper.removeEventListener("mousemove", this.onMove);
     this.wrapper.removeEventListener("mouseleave", this.onEnd);
+    this.dist.finalPosition = this.dist.movePosition;
   }
 
   onStart(event) {
     event.preventDefault();
-    this.dist.startX = event.pageX - this.dist.moviment;
+    this.dist.startX = event.pageX;
     this.wrapper.addEventListener("mousemove", this.onMove);
     this.wrapper.addEventListener("mouseleave", this.onEnd);
   }
